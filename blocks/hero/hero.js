e49e2ad00c254fa9aca7b7dd6935b26633681a20
Milagros Perez <milagros.perez.external@sonova.com>
2023-09-01 12:34:13 -0300

    Adding hero homepage variation styles

---
+++ blocks/hero/hero.js
@@ -0,0 +1,22 @@
+export default function decorate(block) {
+
+  // setup image columns
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 1) {
+          // picture is only content in column
+          picWrapper.classList.add('hero-img-col');
+        } 
+      }
+      
+      const content = row.querySelector('div');
+      if(content) {
+        const contentWrapper = content.closest('div');
+        contentWrapper.classList.add('hero-content-col');
+      }
+    });
+  });
+}
