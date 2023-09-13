671552b1646f681ebe99c5cd91db988628b25600
Carolina Trejos Gutiérrez <carolina.trejos@conexiogroup.com>
2023-09-13 11:32:16 -0600

    Update header.js
    Adding the nav functionality to work with the search

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,4 +1,6 @@
 import { getMetadata, decorateIcons } from '../../scripts/lib-franklin.js';
+import { cleanVariations } from '../../scripts/scripts.js';
+import createTag from '../../utils/tag.js';
 
 // media query match that indicates mobile/tablet width
 const isDesktop = window.matchMedia('(min-width: 900px)');
@@ -85,6 +87,20 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
   }
 }
 
+const toggleSearch = function(){
+  const searchWrapper = document.querySelector('.nav-tools .gnav-search');
+  const searchContainerHidden = document.querySelector('.search-container');
+  const sectionHidden = document.querySelectorAll('.section');
+
+  searchWrapper.classList.toggle('hide');
+  if(searchContainerHidden.querySelector('ul').childElementCount){
+    searchContainerHidden.classList.toggle('hide');
+    sectionHidden.forEach((element) => {
+        element.classList.toggle('hide');
+    });
+  }
+};
+
 /**
  * decorates the header, mainly the nav
  * @param {Element} block The header block element
@@ -96,6 +112,8 @@ export default async function decorate(block) {
   const resp = await fetch(`${navPath}.plain.html`);
 
   if (resp.ok) {
+
+
     const html = await resp.text();
 
     // decorate nav DOM
@@ -123,7 +141,12 @@ export default async function decorate(block) {
         });
       });
     }
+    /* EventListener for seach button */
+    const navTools = nav.querySelector('.nav-tools .icon-search');
 
+    navTools.addEventListener('click', (e) => {
+      toggleSearch();
+    });
 
     // hamburger for mobile
     const hamburger = document.createElement('div');
@@ -144,7 +167,7 @@ export default async function decorate(block) {
     navWrapper.append(nav);
     block.append(navWrapper);
 
-+  // Custom code: adding classes to nav and hiding/showing nav on hover
+  // Custom code: adding classes to nav and hiding/showing nav on hover
     navSections.querySelectorAll('.nav-drop > ul > li').forEach((drop) => {
       drop.querySelector('ul').classList.add('nav--third-level');
       drop.querySelector('.nav--third-level > li > ul').classList.add('active');
@@ -159,7 +182,7 @@ export default async function decorate(block) {
 
 
       drop.querySelectorAll('.nav--third-level > li').forEach((thirdLevel) => {
-  
+
         if(thirdLevel.querySelector('ul') !== null) {
           thirdLevel.addEventListener('mouseenter', (e) => {
             thirdLevel.querySelector('ul').classList.add('active');
@@ -201,5 +224,118 @@ export default async function decorate(block) {
       navTitle.textContent = title.textContent;
       title.replaceWith(navTitle);
     });
+
+    try {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, 'text/html');
+
+      const gnav = new Gnav(doc.body);
+      cleanVariations(doc);
+      gnav.initHeader();
+    } catch (e) {
+      const { debug } = await import('../../utils/console.js');
+      if (debug) {
+        debug('Could not great global navigation', e);
+      }
+    }
   }
+}
+
+const IS_OPEN = 'is-open';
+
+class Gnav {
+  constructor(body, el) {
+    this.el = el;
+    this.body = body;
+    this.desktop = window.matchMedia('(min-width: 1200px)');
+  }
+
+  initHeader = () => {
+    this.state = {};
+
+    const nav = createTag('nav', { class: 'gnav' });
+
+    const mainnav = document.querySelector('.nav-tools');
+
+
+    // disabled search as not in desktop design
+    const enableSearch = getMetadata('enable-search');
+    if (!enableSearch || enableSearch !== 'no') {
+      const div = createTag('div', { class: 'search' });
+      div.innerHTML = '<p>Search</p>';
+      this.body.append(div);
+
+      this.search = this.decorateSearch();
+      if (this.search) {
+        mainnav.append(this.search);
+      }
+    }
+
+
+    window.addEventListener('resize', this.resizeContent);
+  };
+
+  // search on mobile menu (Disabled for now)
+  decorateSearch = () => {
+    const searchBlock = this.body.querySelector('.search');
+    if (searchBlock) {
+      const label = searchBlock.querySelector('p').textContent;
+      const searchEl = createTag('div', { class: 'gnav-search hide' });
+      const searchBar = this.decorateSearchBar(label);
+
+      searchEl.append(searchBar);
+      return searchEl;
+    }
+    return null;
+  };
+
+  decorateSearchBar = (label) => {
+    const searchBar = createTag('aside', { id: 'gnav-search-bar', class: 'gnav-search-bar' });
+    const mainContainer = document.querySelector('main');
+    const searchField = createTag('div', { class: 'gnav-search-field' });
+    const html = `<div class="cards-wrapper">
+                      <div class="cards">
+                        <ul>
+                        </ul>
+                      </div>
+                    </div>`;
+    const containerResults = createTag('div', { class: 'search-container' }, html);
+    const searchInput = createTag('input', { class: 'gnav-search-input', placeholder: label });
+    const searchResults = createTag('div', { class: 'gnav-search-results' });
+
+    const clearButton = createTag('button', { class: 'clear-results-button' }, '✕');
+
+    mainContainer.prepend(containerResults);
+
+    clearButton.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.clearSearchInput();
+    });
+
+
+    searchInput.addEventListener('input', (e) => {
+      if(!this.onSearchInput){
+        this.loadSearchNav();
+      }
+      else{
+        this.onSearchInput(e.target.value, containerResults.querySelector('ul'));
+      }
+
+    });
+
+    searchField.append(searchInput, clearButton);
+    searchBar.append(searchField, searchResults);
+    return searchBar;
+  };
+
+  loadSearchNav = async () => {
+    if (this.onSearchInput) return;
+    const gnavSearch = await import('./gnav-search.js');
+    this.onSearchInput = gnavSearch.default;
+  };
+
+  clearSearchInput = () => {
+    toggleSearch();
+
+  };
 }
