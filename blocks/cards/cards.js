56c7e1fef2395a569fca1ec30fe4eb532e3f12d5
Ignacio Lamberto <ignacio.lamberto@conexiogroup.com>
2023-10-04 16:50:19 -0300

    Styling the Cards block

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -9,10 +9,56 @@ export default function decorate(block) {
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
+      if (div.childNodes.length > 0) {
+        let childNodes = div.childNodes;
+        childNodes.forEach(function (el, i) {
+          if (el.innerHTML) {
+            if (el.innerHTML.indexOf('subheadline') > 0 || el.innerHTML.indexOf('headline') > 0) {
+              let elClass = el.innerHTML.replace(/^.+-/, '');
+              let newText = el.innerHTML.substring(0, el.innerHTML.indexOf('(-'));
+
+              elClass = elClass.slice(0, -1);
+              el.className = elClass;
+              newText = newText.slice(0, -1);
+              el.innerHTML = newText;
+            }
+          }
+
+        });
+      }
+
     });
     ul.append(li);
   });
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
+
+
+  var visible = function (partial, el) {
+
+    var viewTop = window.scrollY + 100,
+      viewBottom = viewTop + window.innerHeight - 100,
+      _top = el.offsetTop,
+      _bottom = _top + el.offsetHeight,
+      compareTop = partial === true ? _bottom : _top,
+      compareBottom = partial === true ? _top : _bottom;
+
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+
+  window.addEventListener('scroll', function (event) {
+    var allElem = document.querySelectorAll('.slideup li');
+    allElem.forEach(function (el, i) {
+      if (visible(true, el)) {
+        el.className = "come-in";
+        el.style.setProperty('--transition-delay', ''.concat(i + 1, '00ms'));
+      }
+    });
+  });
+
+
+
+
 }
